62d2d94e2374e6099ada504e761d8c2ec972dba2
mirian-m <mirianmaglakelidze7@gmail.com>
2023-10-22 11:42:36 +0400

    add navigation items

--- OnlineFitnessTrainerApp/View/Home/HomeViewController.swift
+++ OnlineFitnessTrainerApp/View/Home/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+final class HomeViewController: UIViewController {
     private let heightConst = 220
     private var manager = HomeManager()
     
@@ -24,7 +24,7 @@ final class HomeViewController: UIViewController, UICollectionViewDelegate, UICo
         collectionView.register(FeatureWorkoutCollectionViewCell.self,
                                 forCellWithReuseIdentifier: FeatureWorkoutCollectionViewCell.identifier)
         
-        collectionView.center = view.center
+        collectionView.frame = view.bounds
         collectionView.backgroundColor = .clear
         view.addSubview(collectionView)
         return collectionView
@@ -35,17 +35,44 @@ final class HomeViewController: UIViewController, UICollectionViewDelegate, UICo
         view.backgroundColor = Const.Colors.backgroundColorDark
         featureWorkoutCollectionView.delegate = self
         featureWorkoutCollectionView.dataSource = self
-
+        navigationBarConfigure()
         bind()
         manager.fetchData()
     }
     
+    private func navigationBarConfigure() {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        tabBarController?.navigationController?.navigationBar.isHidden = false
+        navigationController?.navigationBar.tintColor = .clear
+        let image = Const.Icon.goFitIcon
+        let customBtn = UIButton()
+        customBtn.buttonSet(image, "GoFit")
+        let btn = UIBarButtonItem(customView: customBtn)
+        tabBarController?.navigationItem.setLeftBarButton(btn, animated: true)
+        tabBarController?.navigationItem.setRightBarButtonItems([
+            UIBarButtonItem(image: Const.Icon.bookmarkIcon
+                                .withConfiguration(UIImage.SymbolConfiguration(pointSize: 13))
+                                .withTintColor(.white, renderingMode: .alwaysOriginal),
+                            style: .done, target: self, action: nil),
+            
+            UIBarButtonItem(image: Const.Icon.bellIcon
+                                .withConfiguration(UIImage.SymbolConfiguration(pointSize: 13))
+                                .withTintColor(.white, renderingMode: .alwaysOriginal),
+                            style: .done, target: self, action: nil)
+        ], animated: true)
+        
+        tabBarController?.navigationItem.title = "Good Morning 👋"
+        
+    }
     private func bind() {
         manager.workOuts.bind { workOutPlan in
             print(workOutPlan?.first?.difficulty! ?? "")
         }
     }
-    
+}
+
+// MARK:- Extension Delegat methods
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         1
     }
--- OnlineFitnessTrainerApp/View/TabBar/TabBarViewController.swift
+++ OnlineFitnessTrainerApp/View/TabBar/TabBarViewController.swift
@@ -19,35 +19,8 @@ final class TabBarViewController: UITabBarController {
 extension TabBarViewController {
     
     private func congfiguration() {
-        navigationBarConfigure()
         addViewControllersToTabbar()
     }
-    
-    private func navigationBarConfigure() {
-        navigationController?.navigationBar.isHidden = false
-        self.navigationItem.setHidesBackButton(true, animated: true)
-        navigationController?.navigationBar.barTintColor = .clear
-        let image = Const.Icon.goFitIcon
-        let customBtn = UIButton()
-        customBtn.buttonSet(image, "GoFit")
-        let btn = UIBarButtonItem(customView: customBtn)
-        navigationItem.setLeftBarButton(btn, animated: true)
-        navigationItem.setRightBarButtonItems([
-            UIBarButtonItem(image: Const.Icon.bookmarkIcon
-                                .withConfiguration(UIImage.SymbolConfiguration(pointSize: 13))
-                                .withTintColor(.white, renderingMode: .alwaysOriginal),
-                            style: .done, target: self, action: nil),
-            
-            UIBarButtonItem(image: Const.Icon.bellIcon
-                                .withConfiguration(UIImage.SymbolConfiguration(pointSize: 13))
-                                .withTintColor(.white, renderingMode: .alwaysOriginal),
-                            style: .done, target: self, action: nil)
-        ], animated: true)
-        
-        title = "Good Morning 👋"
-        
-    }
-    
     private func addViewControllersToTabbar() {
         let vc = HomeViewController()
         vc.tabBarItem = UITabBarItem(
